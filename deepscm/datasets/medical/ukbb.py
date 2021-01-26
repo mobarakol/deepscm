eb47816c10257dc201dc5748c37885ff5cd2476e
Mobarakol Islam <mi615@mira03.doc.ic.ac.uk>
2021-01-26 12:46:47 +0000

    3d version

--- deepscm/datasets/medical/ukbb.py
+++ deepscm/datasets/medical/ukbb.py
@@ -1,25 +1,21 @@
 from torch.utils.data.dataset import Dataset
-from skimage.io import imread
 import numpy as np
 import pandas as pd
 import os
-
+import nibabel as nib
+from nilearn.image import resample_img
 import torch
-import torchvision as tv
-
 
 class UKBBDataset(Dataset):
-    def __init__(self, csv_path, base_path='/vol/biomedic2/bglocker/gemini/UKBB/t0/', crop_type=None, crop_size=(192, 192), downsample: int = None):
+    def __init__(self, csv_path, base_path='/vol/biobank/12579/brain/rigid_to_mni/images', crop_type=None, crop_size=(64, 64, 64), downsample: float = 2.5):
         super().__init__()
         self.csv_path = csv_path
         df = pd.read_csv(csv_path)
         self.num_items = len(df)
         self.metrics = {col: torch.as_tensor(df[col]).float() for col in df.columns}
         self.base_path = base_path
-
-        self.crop_type = crop_type
-        self.crop_size = crop_size
-
+        self.filename = 'T1_unbiased_brain_rigid_to_mni.nii.gz'
+        self.crop_size = np.array(crop_size)
         self.downsample = downsample
 
     def __len__(self):
@@ -27,27 +23,21 @@ class UKBBDataset(Dataset):
 
     def __getitem__(self, index):
         item = {col: values[index] for col, values in self.metrics.items()}
+        mri_path = os.path.join(self.base_path,str(int(item['eid'])),self.filename)
+        try:
+            img = nib.load(mri_path)#.get_data()
+        except:
+            index += 1
+            item = {col: values[index] for col, values in self.metrics.items()}
+            mri_path = os.path.join(self.base_path,str(int(item['eid'])),self.filename)
+            img = nib.load(mri_path)#.get_data()
 
-        img_path = os.path.join(self.base_path, '{}_T1_unbiased_brain_rigid_to_mni.png'.format(int(item['eid'])))
-        img = imread(img_path, as_gray=True)
-
-        transform_list = []
-        transform_list += [tv.transforms.ToPILImage()]
-        if self.crop_type is not None:
-            if self.crop_type == 'center':
-                transform_list += [tv.transforms.CenterCrop(self.crop_size)]
-            elif self.crop_type == 'random':
-                transform_list += [tv.transforms.RandomCrop(self.crop_size)]
-            else:
-                raise ValueError('unknwon crop type: {}'.format(self.crop_type))
-
-        if self.downsample is not None and self.downsample > 1:
-            transform_list += [tv.transforms.Resize(tuple(np.array(self.crop_size) // self.downsample))]
-
-        transform_list += [tv.transforms.ToTensor()]
-
-        img = tv.transforms.Compose(transform_list)(img)
-
-        item['image'] = img
-
+        downsampled_nii = resample_img(img, target_affine=np.eye(3)*self.downsample, interpolation='linear')
+        img = downsampled_nii.dataobj
+        init_pos = np.round(np.array(img.shape)/2-self.crop_size/2).astype(int)
+        end_pos = init_pos+self.crop_size
+        min_ = np.min(img)
+        max_ = np.max(img)
+        img = (img - min_) / (max_ - min_)
+        item['image'] = np.expand_dims(img[init_pos[0]:end_pos[0],init_pos[1]:end_pos[1],init_pos[2]:end_pos[2]], axis=0)
         return item
